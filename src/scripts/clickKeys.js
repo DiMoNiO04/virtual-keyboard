e9d43a5c33731b57f1c6cdb4a24c24caa865f951
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-04-30 22:37:40 +0300

    feat: change language and show keyboard and refactor arr keys

--- src/scripts/clickKeys.js
+++ src/scripts/clickKeys.js
@@ -1,9 +1,10 @@
 import { textarea } from "./createKeyboard";
-import { showKeyboard } from "./showKeyboard";
-const keys = document.querySelectorAll('.key'); 
+import { createKeyboard } from "./showKeyboard";
+import { keys } from "./keys";
+const keysHTML = document.querySelectorAll('.key'); 
 
 document.addEventListener('keydown', (event) => {
-	keys.forEach(function(key) {
+	keysHTML.forEach(function(key) {
 		if( event.code == key.getAttribute('data-key') ) {
 			key.classList.add('active');
 			if( event.code === 'CapsLock' || event.code === 'Space' || 
@@ -16,13 +17,18 @@ document.addEventListener('keydown', (event) => {
 				return;
 			}
 			textarea.textContent += event.key;
+			console.log(event.keyCode)
 		}
 	})
 })
 
+
 document.addEventListener('keyup', (event) => {
-	keys.forEach(function(key) {
+	keysHTML.forEach(function(key) {
 		if( event.code == key.getAttribute('data-key') ) {
+			if( event.code === 'CapsLock' ) {
+				return;
+			}
 			setTimeout(() => {
 				key.classList.remove('active')
 			}, 100)
@@ -30,3 +36,33 @@ document.addEventListener('keyup', (event) => {
 	})
 })
 
+
+const changeKeyboard = (language) => {
+	for(let i = 0; i < keysHTML.length; i++) {
+		keysHTML[i].textContent = keys[i][language]
+	}
+}
+
+// document.addEventListener('keydown', (event) => {
+// 	if(event.altKey && event.shiftKey) {
+// 		changeKeyboard('keyRuDown')
+// 	}
+// })
+
+// document.addEventListener('keydown', (event) => {
+// 	if(event.code === document.querySelector('[data-key="CapsLock"]').getAttribute('data-key')) {
+// 		changeKeyboard('keyEnCaps');
+// 	}
+// })
+
+// document.addEventListener('keyup', (event) => {
+// 	if(event.code === document.querySelector('[data-key="ShiftLeft"]').getAttribute('data-key')) {
+// 		changeKeyboard('keyEn');
+// 	}
+// })
+
+// document.addEventListener('keyup', (event) => {
+// 	if(event.code === document.querySelector('[data-key="CapsLock"]').getAttribute('data-key')) {
+// 		changeKeyboard('keyEn');
+// 	}
+// })
--- src/scripts/showKeyboard.js
+++ src/scripts/showKeyboard.js
@@ -1,12 +1,95 @@
 import { keys } from './keys';
 import { keyboardBody } from './createKeyboard';
 
-export const showKeyboard = (language) => {
+export const createKeyboard = () => {
   let out = '';
-  for (let key = 0; key < keys.length; key += 1) {
-    out += `<div class="key" data-key="${keys[key].code}">${keys[key][language]}</div>`;
-  }
+	keys.forEach(function(key) {
+		out += `
+			<div class="key" data-key="${key.code}">
+				<div class="en">
+					<span class="down hidden">${key['keyEnDown']}</span>
+					<span class="up hidden">${key['keyEnUp']}</span>
+					<span class="caps hidden">${key['keyEnCaps']}</span>
+					<span class="shiftCaps hidden">${key['keyEnShiftCaps']}</span>
+				</div>
+				<div class="ru">
+					<span class="down hidden">${key['keyRuDown']}</span>
+					<span class="up hidden">${key['keyRuUp']}</span>
+					<span class="caps hidden">${key['keyRuCaps']}</span>
+					<span class="shiftCaps hidden">${key['keyRuShiftCaps']}</span>
+				</div>
+			</div>
+			`;
+	})
   keyboardBody.innerHTML = out;
 };
 
-document.addEventListener('DOMContentLoaded', showKeyboard('keyEn'));
+
+
+document.addEventListener('DOMContentLoaded', createKeyboard());
+
+
+const EN = 'en';
+const RU = 'ru';
+
+//Работа с localeStorage 
+let language = EN;
+
+const getLanguageFromLocalestorage = () => {
+	if(localStorage.getItem('language')) {
+		language = localStorage.getItem('language')
+	}
+	return language;
+}
+
+const setLocaleStorage = (language) => localStorage.setItem('language', language)
+document.addEventListener('load', setLocaleStorage(getLanguageFromLocalestorage()))
+
+
+
+//Смена раскладки клавиатуры
+const EN_ELEM = document.querySelectorAll('.en');
+const RU_ELEM = document.querySelectorAll('.ru');
+
+function keyboardChange(language, languageClass) {
+	for(let i = 0; i < language.length; i++) {
+		if(languageClass === RU) {
+			let keyElemRu = RU_ELEM[i];
+			keyElemRu.children[0].classList.remove('hidden')
+
+			let keyElemEn = EN_ELEM[i];
+			keyElemEn.children[0].classList.add('hidden')
+		} else {
+			let keyElemRu = RU_ELEM[i];
+			keyElemRu.children[0].classList.add('hidden')
+
+			let keyElemEn = EN_ELEM[i];
+			keyElemEn.children[0].classList.remove('hidden')
+		}
+	}
+}
+
+const showKeyboard = (language) => {
+	if( localStorage.getItem('language') === EN) {
+		keyboardChange(EN_ELEM, EN)
+	} else {
+		keyboardChange(RU_ELEM, RU)
+	}
+}
+
+document.addEventListener('load', showKeyboard(language))
+
+
+//Обработка оптициональных клавиш
+const changeLanguage = (event) => {
+	if( event.altKey && event.ctrlKey ) {
+		if(getLanguageFromLocalestorage() === EN) {
+			language = RU;
+		} else {
+			language = EN
+		}
+		setLocaleStorage(language);
+		showKeyboard(language)
+	}
+}
+window.addEventListener('keydown', changeLanguage)
